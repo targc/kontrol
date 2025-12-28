5b17fae61c8b44f3946ff404f9b3a9e4d45c402c
targc <sommit.kcc@gmail.com>
2025-12-28 11:00:34 +0700

    refactor:

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,14 +8,15 @@ import (
 )
 
 type Config struct {
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
-	ClusterID  string
-	ServerPort string
-	Kubeconfig string
+	DBHost      string
+	DBPort      string
+	DBUser      string
+	DBPassword  string
+	DBName      string
+	TablePrefix string
+	ClusterID   string
+	ServerPort  string
+	Kubeconfig  string
 }
 
 func Load() *Config {
@@ -24,14 +25,15 @@ func Load() *Config {
 	}
 
 	return &Config{
-		DBHost:     getEnv("KONTROL_DB_HOST", "localhost"),
-		DBPort:     getEnv("KONTROL_DB_PORT", "5432"),
-		DBUser:     getEnv("KONTROL_DB_USER", "postgres"),
-		DBPassword: getEnv("KONTROL_DB_PASSWORD", "postgres"),
-		DBName:     getEnv("KONTROL_DB_NAME", "kontrol"),
-		ClusterID:  getEnv("KONTROL_CLUSTER_ID", "default"),
-		ServerPort: getEnv("KONTROL_SERVER_PORT", "8080"),
-		Kubeconfig: getEnv("KONTROL_KUBECONFIG", os.Getenv("HOME")+"/.kube/config"),
+		DBHost:      getEnv("KONTROL_DB_HOST", "localhost"),
+		DBPort:      getEnv("KONTROL_DB_PORT", "5432"),
+		DBUser:      getEnv("KONTROL_DB_USER", "postgres"),
+		DBPassword:  getEnv("KONTROL_DB_PASSWORD", "postgres"),
+		DBName:      getEnv("KONTROL_DB_NAME", "kontrol"),
+		TablePrefix: getEnv("KONTROL_TABLE_PREFIX", ""),
+		ClusterID:   getEnv("KONTROL_CLUSTER_ID", "default"),
+		ServerPort:  getEnv("KONTROL_SERVER_PORT", "8080"),
+		Kubeconfig:  getEnv("KONTROL_KUBECONFIG", os.Getenv("HOME")+"/.kube/config"),
 	}
 }
 
--- pkg/database/database.go
+++ pkg/database/database.go
@@ -9,6 +9,7 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 )
 
 func Connect(cfg *config.Config) (*gorm.DB, error) {
@@ -19,12 +20,13 @@ func Connect(cfg *config.Config) (*gorm.DB, error) {
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
+		NamingStrategy: schema.NamingStrategy{
+			TablePrefix: cfg.TablePrefix,
+		},
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
-
-	log.Println("Database connected successfully")
 
 	return db, nil
 }
